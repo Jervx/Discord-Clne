231c66d344ea08db46b0b615e9b64673ccb648f2
Jervx <louellagracechua@gmail.com>
2023-09-29 22:05:44 +0800

    Sidebar | Navigation

---
+++ components/file-upload.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import "@uploadthing/react/styles.css";
+import { UploadDropzone } from "@/lib/uploadthing";
+
+interface FileUploadProps {
+    onChange: (url?: string) => void;
+    value: string;
+    endpoint: "messageFile" | "serverImage";
+}
+
+const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+    const fileType = value.split(".").pop();
+    if (value && fileType !== "pdf") {
+        return (
+            <div className="relative w-20 h-20">
+                <Image fill src={value} alt="upload" className="rounded-full" />
+                <button onClick={()=>onChange("")} className="absolute top-0 right-0 p-1 text-white rounded-full shadow-sm bg-rose-500" type="button"><X className="w-4 h-4"/></button>
+            </div>
+        );
+    }
+
+    return (
+        <UploadDropzone
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error);
+            }}
+        />
+    );
+};
+
+export default FileUpload;
