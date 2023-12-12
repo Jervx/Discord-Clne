7d408cccc8d85028ab5276800409c18ac7d9400d
Jervx <louellagracechua@gmail.com>
2023-12-12 15:28:59 +0800

    Feat : Leave & Delete Server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,7 +2,10 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req: Request, { params }: { params : { serverId: string } }) {
+export async function PATCH(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
     try {
         const profile = await currentProfile();
         const { name, imageUrl } = await req.json();
@@ -20,6 +23,23 @@ export async function PATCH(req: Request, { params }: { params : { serverId: str
         return NextResponse.json(server);
     } catch (error) {
         console.log("[SERVERID_PATCH]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
+
+export async function DELETE( req: Request, { params }: { params: { serverId: string } }) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) return new NextResponse("UNAUTHORIZED", { status: 401 });
+
+        const server = await db.server.delete({
+            where: { id: params.serverId, profileId: profile.id },
+        });
+
+        return NextResponse.json(server);
+    } catch (error) {
+        console.log("[SERVERID_DELETE]", error);
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
---
+++ components/modals/leave-server-modal.tsx
@@ -0,0 +1,65 @@
+"use client";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+
+import { useModal } from "../../hooks/use-modal-store";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export const LeaveServer = () => {
+    const { isOpen, onOpen, onClose, type, data } = useModal();
+    const router = useRouter();
+
+    const isModalOpen = isOpen && type === "leaveServer";
+    const { server } = data;
+
+    const [copied, setCopied] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onClick = async () => {
+        try{
+            setIsLoading(true)
+            await axios.patch(`/api/servers/${server?.id}/leave`)
+            
+            onClose();
+            router.refresh();
+            router.push("/")
+        }catch(e){
+            console.log(e)
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <Dialog open={isModalOpen} onOpenChange={onClose}>
+            <DialogContent className="p-0 overflow-hidden text-black bg-white">
+                <DialogHeader className="px-6 pt-8">
+                    <DialogTitle className="text-2xl font-bold text-center text-zinc">
+                        Leave Server
+                    </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        Are you sure you want to leave <span className="font-semibold text-indigo-500">{server?.name}</span>?
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="px-6 py-4 bg-gray-100">
+                <div className="flex items-center justify-between w-full">
+                    <Button disabled={isLoading} onClick={()=> onClose() } variant={"ghost"}>Cancel</Button>
+                    <Button disabled={isLoading} onClick={onClick} variant={"primary"}>Confirm</Button>
+                </div>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
